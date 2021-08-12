d0705873911956f27b26d2aae7d4bd5d1cca4436
sindhuM21 <82466289+sindhuM21@users.noreply.github.com>
2021-08-12 23:15:04 +0530

    Update FileNode.js

--- FileNode.js
+++ FileNode.js
@@ -1,1 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+const { title } = require("process");
 
+const server = http.createServer(function(req, res){
+    if(req.url == "/"){
+        res.write("<html>");
+        res.write("<head><title>welcome</title></head>");
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'></input><button type='submit'>Send</button></form></body>")
+        res.write("</html>")
+        return res.end();
+    }
+    if(req.url == "/message" && req.method === "POST"){
+        const body = [];
+        req.on("data", (chunk) => {
+            console.log("chunkkkk", chunk);
+            body.push(chunk);
+        });
+        req.on("end", () => {
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split("=")[1];
+            fs.writeFileSync("message.txt", message)
+            console.log("parseBody", parseBody); 
+        })
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+     res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
+})
+
+server.listen(3000);
